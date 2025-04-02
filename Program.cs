2bf71a18a921016929ee2dcbceb757754b54bb7a
Kleber Neves <klebernssii@gmail.com>
2025-04-02 12:27:43 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double milhas,km;
+
+Console.Write("Valor em milhas:");
+milhas = Convert.ToDouble(Console.ReadLine()!);
+
+
+Console.Clear();
+Console.WriteLine($"valor em milhas {milhas}");
+Console.Write("Valor em km");
+km = milhas * 1*1.609;
+Console.WriteLine($"{km}");
